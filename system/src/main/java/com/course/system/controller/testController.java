c2ec9a91a4a41c22cdccc6e2d0279476d2a9a1ea
chenzquan <122705737@qq.com>
2020-07-27 00:19:19 +0800

    集成mybatis

--- system/src/main/java/com/course/system/controller/testController.java
+++ system/src/main/java/com/course/system/controller/testController.java
@@ -1,18 +1,27 @@
 package com.course.system.controller;
 
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @RestController
 public class testController {
 
+    @Resource
+    private TestService testService;
 
-    @GetMapping("/test")
-    public String test(){
-        return "success";
+
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.list();
     }
 
 }
