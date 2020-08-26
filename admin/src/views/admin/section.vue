1b475e85388055ab6e3f5c5ad25317dbf362f8d5
chenzquan <122705737@qq.com>
2020-08-26 21:08:51 +0800

    增加了枚举常量

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -196,7 +196,7 @@
             return {
                 sections: [],
                 section:{},
-                CHARGE:[{key:"C",value:"收费"},{key:"F",value:"免费"}]
+                CHARGE:CHARGE
             }
         },
         mounted() {
