1c8614776b46ec6f20ebf24be564c7da797a37cf
chenzquan <122705737@qq.com>
2020-09-08 23:38:21 +0800

    课程列表中显示讲师信息 课程名称字体自适应

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -27,6 +27,18 @@
                         <h3 class="search-title">
                             <a href="#" class="blue">{{course.name}}</a>
                         </h3>
+
+                        <div v-for="teacher in teachers.filter(t=>{return t.id===course.teacherId})" class="profile-activity clearfix">
+                            <div>
+                                <img v-show="!teacher.image" class="pull-left"  src="/ace/assets/images/avatars/avatar5.png">
+                                <img v-show="teacher.image" class="pull-left"  :src="teacher.image">
+                                <a class="user" href="#">{{teacher.name}}</a>
+                                <br>
+                                {{teacher.position}}
+                            </div>
+                        </div>
+
+
                         <p>
                             <span class="blue bolder bigger-150">{{course.price}}&nbsp;<i class="fa fa-rmb"></i></span>
                         </p>
@@ -591,6 +603,14 @@
     .caption h3 {
         font-size: 20px;
     }
+
+    /*根据不同分辨率 显示不同大小的字体*/
+    @media (max-width: 1199px) {
+        .caption h3{
+            font-size: 16px;
+        }
+    }
+
 </style>
 
 
