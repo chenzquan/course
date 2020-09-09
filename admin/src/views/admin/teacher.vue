8d474123266601b09fb5928531e51675856c1a88
chenzquan <122705737@qq.com>
2020-09-09 21:41:53 +0800

    文件上传功能开发 springboot+vue基本的文件上传功能，上传讲师头像

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -85,6 +85,7 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
+                  <input type="file" id="file-upload-input" @change="uploadImage()">
 <!--                  <big-file v-bind:input-id="'image-upload'"-->
 <!--                        v-bind:text="'上传头像'"-->
 <!--                        v-bind:suffixs="['jpg', 'jpeg', 'png']"-->
@@ -244,6 +245,20 @@
         let _this = this;
         let image = resp.content.path;
         _this.teacher.image = image;
+      },
+
+      uploadImage(){
+        let _this = this;
+        let formData = new window.FormData();
+        formData.append('file',document.querySelector('#file-upload-input').files[0]);
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload',formData).then((response)=>{
+          Loading.hide();
+          let resp = response.data;
+
+
+
+        });
       }
     }
   }
