5567ee60edd3c2e0116eea05b502e1a1c61eb598
chenzquan <122705737@qq.com>
2020-09-10 23:16:40 +0800

    文件上传功能开发，增加文件大小格式化过滤器

--- admin/src/views/admin/file.vue
+++ admin/src/views/admin/file.vue
@@ -2,10 +2,10 @@
     <div>
         <p>
 
-            <button @click="add()" class="btn btn-white btn-default btn-round">
-                <i class="ace-icon fa fa-edit"></i>新增
-            </button>
-            &nbsp;
+<!--            <button @click="add()" class="btn btn-white btn-default btn-round">-->
+<!--                <i class="ace-icon fa fa-edit"></i>新增-->
+<!--            </button>-->
+<!--            &nbsp;-->
             <button @click="list(1)" class="btn btn-white btn-default btn-round">
                 <i class="ace-icon fa fa-refresh"></i>刷新
             </button>
@@ -21,10 +21,10 @@
              <th>大小</th>
              <th>用途</th>
 
-            <th>操作</th>
+<!--            <th>操作</th>-->
 
 
-            <th></th>
+
             </tr>
             </thead>
 
@@ -35,128 +35,69 @@
                 <td>{{file.path}}</td>
                 <td>{{file.name}}</td>
                 <td>{{file.suffix}}</td>
-                <td>{{file.size}}</td>
+                <td>{{file.size | formatFileSize}}</td>
                 <td>{{FILE_USE | optionKV(file.use)}}</td>
-
-
-            <!--            <td>Feb 12</td>-->
-
-
-            <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                    <!--                        <button class="btn btn-xs btn-success">-->
-                    <!--                            <i class="ace-icon fa fa-check bigger-120"></i>-->
-                    <!--                        </button>-->
-
-                    <button class="btn btn-xs btn-info" @click="edit(file)">
-                        <i class="ace-icon fa fa-pencil bigger-120"></i>
-                    </button>
-
-                    <button class="btn btn-xs btn-danger" @click="deleteData(file.id)">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                    </button>
-
-                    <!--                        <button class="btn btn-xs btn-warning">-->
-                    <!--                            <i class="ace-icon fa fa-flag bigger-120"></i>-->
-                    <!--                        </button>-->
-                </div>
-
-                <div class="hidden-md hidden-lg">
-                    <div class="inline pos-rel">
-                        <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown"
-                                data-position="auto">
-                            <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                        </button>
-
-                        <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                            <li>
-                                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                                                                    <span class="blue">
-                                                                        <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                                                                    </span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                                                                    <span class="green">
-                                                                        <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                                                                    </span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                                                                    <span class="red">
-                                                                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                                                                    </span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </td>
             </tr>
 
             </tbody>
         </table>
 
-        <div class="modal fade" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">表单</h4>
-                    </div>
-                    <div class="modal-body">
-                        <!--                    <p>One fine body&hellip;</p>-->
-                        <form class="form-horizontal">
+<!--        <div class="modal fade" tabindex="-1" role="dialog">-->
+<!--            <div class="modal-dialog" role="document">-->
+<!--                <div class="modal-content">-->
+<!--                    <div class="modal-header">-->
+<!--                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span-->
+<!--                            aria-hidden="true">&times;</span></button>-->
+<!--                        <h4 class="modal-title">表单</h4>-->
+<!--                    </div>-->
+<!--                    <div class="modal-body">-->
+<!--                        &lt;!&ndash;                    <p>One fine body&hellip;</p>&ndash;&gt;-->
+<!--                        <form class="form-horizontal">-->
 
-                                    <div class="form-group">
-                                        <label  class="col-sm-2 control-label">相对路径</label>
-                                        <div class="col-sm-10">
-                                            <input v-model="file.path" class="form-control"  placeholder="相对路径">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label  class="col-sm-2 control-label">文件名</label>
-                                        <div class="col-sm-10">
-                                            <input v-model="file.name" class="form-control"  placeholder="文件名">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label  class="col-sm-2 control-label">后缀</label>
-                                        <div class="col-sm-10">
-                                            <input v-model="file.suffix" class="form-control"  placeholder="后缀">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label  class="col-sm-2 control-label">大小</label>
-                                        <div class="col-sm-10">
-                                            <input v-model="file.size" class="form-control"  placeholder="大小">
-                                        </div>
-                                    </div>
-                            <div class="form-group">
-                                <label  class="col-sm-2 control-label">用途</label>
-                                <div class="col-sm-10">
-                                    <select v-model="file.use" class="form-control">
-                                        <option v-for="o in FILE_USE" :value="o.key">{{o.value}}</option>
-                                    </select>
-                                </div>
-                            </div>
+<!--                                    <div class="form-group">-->
+<!--                                        <label  class="col-sm-2 control-label">相对路径</label>-->
+<!--                                        <div class="col-sm-10">-->
+<!--                                            <input v-model="file.path" class="form-control"  placeholder="相对路径">-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                    <div class="form-group">-->
+<!--                                        <label  class="col-sm-2 control-label">文件名</label>-->
+<!--                                        <div class="col-sm-10">-->
+<!--                                            <input v-model="file.name" class="form-control"  placeholder="文件名">-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                    <div class="form-group">-->
+<!--                                        <label  class="col-sm-2 control-label">后缀</label>-->
+<!--                                        <div class="col-sm-10">-->
+<!--                                            <input v-model="file.suffix" class="form-control"  placeholder="后缀">-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                    <div class="form-group">-->
+<!--                                        <label  class="col-sm-2 control-label">大小</label>-->
+<!--                                        <div class="col-sm-10">-->
+<!--                                            <input v-model="file.size" class="form-control"  placeholder="大小">-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                            <div class="form-group">-->
+<!--                                <label  class="col-sm-2 control-label">用途</label>-->
+<!--                                <div class="col-sm-10">-->
+<!--                                    <select v-model="file.use" class="form-control">-->
+<!--                                        <option v-for="o in FILE_USE" :value="o.key">{{o.value}}</option>-->
+<!--                                    </select>-->
+<!--                                </div>-->
+<!--                            </div>-->
 
 
 
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                        <button type="button" class="btn btn-primary" @click="save()">保存</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
+<!--                        </form>-->
+<!--                    </div>-->
+<!--                    <div class="modal-footer">-->
+<!--                        <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>-->
+<!--                        <button type="button" class="btn btn-primary" @click="save()">保存</button>-->
+<!--                    </div>-->
+<!--                </div>&lt;!&ndash; /.modal-content &ndash;&gt;-->
+<!--            </div>&lt;!&ndash; /.modal-dialog &ndash;&gt;-->
+<!--        </div>&lt;!&ndash; /.modal &ndash;&gt;-->
 
     </div>
 
@@ -214,111 +155,77 @@
                 });
             },
 
-            add() {
-                let _this = this;// eslint-disable-line no-unused-vars
-                _this.file = {};
-                $(".modal").modal("show");
-            },
+            // add() {
+            //     let _this = this;// eslint-disable-line no-unused-vars
+            //     _this.file = {};
+            //     $(".modal").modal("show");
+            // },
+            //
+            // edit(file){
+            //     let _this = this;
+            //     $(".modal").modal("show");
+            //     _this.file = $.extend({},file);
+            // },
 
-            edit(file){
-                let _this = this;
-                $(".modal").modal("show");
-                _this.file = $.extend({},file);
-            },
+            // deleteData(id){
+            //     let _this = this;
+            //     Confirm.show("删除文件后不可恢复，确认删除？",function () {
+            //         Loading.show();
+            //         _this.$ajax.delete(process.env.VUE_APP_SERVER + '/file/admin/file/delete/' + id).then((response) => {
+            //             Loading.hide();
+            //             let res = response.data;
+            //             console.log("jieguo add", response);
+            //             if(res.success){
+            //                 // $(".modal").modal("hide");
+            //                 _this.list(1);
+            //                 Toast.success("删除成功!")
+            //             }
+            //             // _this.files = response.data.list;
+            //             // _this.$refs.pagination.render(page, response.data.total);
+            //
+            //         });
+            //     });
+            //
+            // },
 
-            deleteData(id){
-                let _this = this;
-                Confirm.show("删除文件后不可恢复，确认删除？",function () {
-                    Loading.show();
-                    _this.$ajax.delete(process.env.VUE_APP_SERVER + '/file/admin/file/delete/' + id).then((response) => {
-                        Loading.hide();
-                        let res = response.data;
-                        console.log("jieguo add", response);
-                        if(res.success){
-                            // $(".modal").modal("hide");
-                            _this.list(1);
-                            Toast.success("删除成功!")
-                        }
-                        // _this.files = response.data.list;
-                        // _this.$refs.pagination.render(page, response.data.total);
-
-                    });
-                });
-
-                // Swal.fire({
-                //     title: '确认删除?',
-                //     text: '删除后不可恢复，确认删除',
-                //     icon: 'warning',
-                //     showCancelButton: true,
-                //     confirmButtonText: '确认',
-                //     cancelButtonText: '取消'
-                // }).then((result) => {
-                //     if (result.value) {
-                //         Loading.show();
-                //         _this.$ajax.delete('http://127.0.0.1:9000/file/admin/file/delete/' + id).then((response) => {
-                //             Loading.hide();
-                //             let res = response.data;
-                //             console.log("jieguo add", response);
-                //             if(res.success){
-                //                 // $(".modal").modal("hide");
-                //                 _this.list(1);
-                //                 Toast.success("删除成功!")
-                //             }
-                //             // _this.files = response.data.list;
-                //             // _this.$refs.pagination.render(page, response.data.total);
-                //
-                //         });
-                //     }
-                //     // else if (result.dismiss === Swal.DismissReason.cancel) {
-                //     //     Swal.fire(
-                //     //         'Cancelled',
-                //     //         'Your imaginary file is safe :)',
-                //     //         'error'
-                //     //     )
-                //     // }
-                // });
-
-
-            },
-
-            save(){
-                let _this = this;
-
-                if (1!=1
-                                   || !Validator.request(_this.file.path,"相对路径")
-                   || !Validator.length(_this.file.path,"相对路径",1,100)
-                                   || !Validator.length(_this.file.name,"文件名",1,100)
-                                   || !Validator.length(_this.file.suffix,"后缀",1,10)
-                                                   || !Validator.length(_this.file.use,"用途",1,50)
-                                                ){
-                    return;
-                }
-
-                //保存校验
-                // if (!Validator.request(_this.file.name,"名称")
-                //     || !Validator.request(_this.file.courseId,"课程ID")
-                //     || !Validator.length(_this.file.courseId,"课程ID",1,8)){
-                //     return;
-                // }
-
-
-                _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/save',
-                    _this.file
-                ).then((response) => {
-                    let res = response.data;
-                    console.log("jieguo add", response);
-                    if(res.success){
-                        $(".modal").modal("hide");
-                        _this.list(1);
-                        Toast.success("保存成功!");
-                    }else{
-                        Toast.warning(res.message);
-                    }
-                    // _this.files = response.data.list;
-                    // _this.$refs.pagination.render(page, response.data.total);
-
-                });
-            },
+            // save(){
+            //     let _this = this;
+            //
+            //     if (1!=1
+            //                        || !Validator.request(_this.file.path,"相对路径")
+            //        || !Validator.length(_this.file.path,"相对路径",1,100)
+            //                        || !Validator.length(_this.file.name,"文件名",1,100)
+            //                        || !Validator.length(_this.file.suffix,"后缀",1,10)
+            //                                        || !Validator.length(_this.file.use,"用途",1,50)
+            //                                     ){
+            //         return;
+            //     }
+            //
+            //     //保存校验
+            //     // if (!Validator.request(_this.file.name,"名称")
+            //     //     || !Validator.request(_this.file.courseId,"课程ID")
+            //     //     || !Validator.length(_this.file.courseId,"课程ID",1,8)){
+            //     //     return;
+            //     // }
+            //
+            //
+            //     _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/save',
+            //         _this.file
+            //     ).then((response) => {
+            //         let res = response.data;
+            //         console.log("jieguo add", response);
+            //         if(res.success){
+            //             $(".modal").modal("hide");
+            //             _this.list(1);
+            //             Toast.success("保存成功!");
+            //         }else{
+            //             Toast.warning(res.message);
+            //         }
+            //         // _this.files = response.data.list;
+            //         // _this.$refs.pagination.render(page, response.data.total);
+            //
+            //     });
+            // },
 
 
 
