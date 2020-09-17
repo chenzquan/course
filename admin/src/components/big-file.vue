d2280ba8c2f13f943271d9230045b00b37ef3e0a
chenzquan <122705737@qq.com>
2020-09-17 23:26:12 +0800

    大文件断点续传，增加big-file组件，分片传输测试成功

---
+++ admin/src/components/big-file.vue
@@ -0,0 +1,116 @@
+<template>
+    <div>
+        <button type="button" v-on:click="selectFile()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-upload "></i>
+            {{text}}
+        </button>
+        <input class="hidden" type="file" ref="file" :id="inputId + '-input'" @change="uploadFile()">
+        <!--                  <big-file v-bind:input-id="'image-upload'"-->
+        <!--                        v-bind:text="'上传头像'"-->
+        <!--                        v-bind:suffixs="['jpg', 'jpeg', 'png']"-->
+        <!--                        v-bind:use="FILE_USE.TEACHER.key"-->
+        <!--                        v-bind:after-upload="afterUpload">-->
+        <!--                    -->
+        <!--                  </big-file>-->
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "big-file",
+        props:{
+            text:{
+                default:"上传大文件"
+            },
+
+            afterUpload:{
+                type:Function,
+                default: null
+            },
+
+            inputId:{
+                default:"file-upload"
+            },
+
+            suffixs:{
+                default:[]
+            },
+            use:{
+                default:""
+            }
+
+        },
+        data:function () {
+            return{
+
+            }
+        },
+
+        methods:{
+            uploadFile(){
+                let _this = this;
+                let formData = new window.FormData();
+
+                let file = _this.$refs.file.files[0];
+
+                //判断 文件格式
+
+                let suffixs = _this.suffixs;
+                let fileName = file.name;
+                let suffix = fileName.substring(fileName.lastIndexOf(".")+1,fileName.length).toLowerCase();
+                let validateSuffix = false;
+                for(let i=0; i<suffixs.length; i++){
+                    if(suffixs[i].toLowerCase() === suffix){
+                        validateSuffix = true;
+                        break;
+                    }
+                }
+
+                if(!validateSuffix){
+                    Toast.warning("文件格式不正确！只支持上传：" + suffixs.join(","));
+                    $("#" + _this.inputId + "-input").val("");
+                    return;
+                }
+
+
+                //文件分片
+                let shardSize = 20 * 1024 * 1024;  //以20MB为一个分片
+
+                let shardIndex = 1;
+                let start = shardSize * shardIndex; //当前分片启始 位置
+                let end = Math.min(file.size,start+shardSize);//当前分片结束位置
+                let fileShard  = file.slice(start,end);
+
+
+
+                // formData.append('file',document.querySelector('#file-upload-input').files[0]);
+                formData.append('file',fileShard);
+                formData.append("use",_this.use);
+                Loading.show();
+                _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload',formData).then((response)=>{
+                    Loading.hide();
+                    let resp = response.data;
+                    let image = resp.content;
+                    // console.log("image",image);
+                    // _this.teacher.image = image;
+                    _this.afterUpload(resp);
+                    $("#" + _this.inputId + "-input").val("");
+                });
+            },
+
+            selectFile(){
+                let _this = this;
+                // $("#file-upload-input").trigger("click");
+                $("#" + _this.inputId + "-input").trigger("click");
+            }
+
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -160,12 +160,12 @@
                                 <label  class="col-sm-2 control-label">视频</label>
                                 <div class="col-sm-10">
 
-                                    <file :text="'上传视频'"
+                                    <big-file :text="'上传大视频'"
                                           :after-upload="afterUpload"
                                           :input-id="'video-upload'"
                                           :suffixs="['mp4']"
                                           :use="FILE_USE.COURSE.key">
-                                    </file>
+                                    </big-file>
                                     <div v-show="section.video" class="row">
                                         <div class="col-md-9">
                                             <video id="video" v-bind:src="section.video" controls="controls"></video>
@@ -219,11 +219,12 @@
     import Pagination from '../../components/pagination.vue';
     // import Swal from 'sweetalert2';
     import File from "../../components/file";
+    import BigFile from '../../components/big-file.vue';
 
     export default {
         name: "business-section",
         components: {
-            Pagination,File
+            Pagination,File,BigFile
         },
 
         data() {
