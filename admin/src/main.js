a1394281c8918b404f17082870abd6bfa16e879e
chenzquan <122705737@qq.com>
2020-10-25 23:37:17 +0800

    用户管理与登录: 在请求headers中统一增加token

--- admin/src/main.js
+++ admin/src/main.js
@@ -17,12 +17,12 @@ axios.defaults.withCredentials = true;
  * axios拦截器
  */
 axios.interceptors.request.use(function (config) {
-  // console.log("请求：", config);
-  // let token = Tool.getLoginUser().token;
-  // if (Tool.isNotEmpty(token)) {
-  //   config.headers.token = token;
-  //   console.log("请求headers增加token:", token);
-  // }
+  console.log("请求：", config);
+  let token = Tool.getLoginUser().token;
+  if (Tool.isNotEmpty(token)) {
+    config.headers.token = token;
+    console.log("请求headers增加token:", token);
+  }
   return config;
 }, error => {});
 axios.interceptors.response.use(function (response) {
