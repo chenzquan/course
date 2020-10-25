8dd4f839af3f073e99f71bc98e93f3a97e022285
chenzquan <122705737@qq.com>
2020-10-25 23:30:12 +0800

    用户管理与登录: 基于vue路由的登录拦截

--- admin/src/router.js
+++ admin/src/router.js
@@ -34,6 +34,10 @@ export default new Router({
         path: '/',
         name:"admin",
         component:Admin,
+        meta:{
+          loginRequire:true
+        },
+
         children:[{
             path: 'welcome',
             name:"welcome",
